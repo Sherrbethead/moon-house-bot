c2984abde8291ec332eedf816bab2040e1f6a3e7
Sergey Bakanov <bakanovsergs@gmail.com>
2021-04-19 22:21:21 +0300

    Add dotenv support

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ from dynaconf import Dynaconf
 
 settings = Dynaconf(
     envvar_prefix=False,
-    load_dotenv=True,
     settings_files=['settings.toml', '.secrets.toml'],
+    load_dotenv=True,
 )
 settings.webhook.path = f'/{settings.token}'
